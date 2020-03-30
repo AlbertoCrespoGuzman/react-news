5b16415a8f3adb101498ce43ced5d3b941f6725f
Alberto Crespo <albertocrespo@Albertos-MacBook-Pro.local>
2020-03-30 18:23:02 +0200

    build production

---
+++ build/precache-manifest.2ec900cea2462181baa5a81832463a7e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bbd69a6ba5e4ed58873995e950486f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67f927bd354e9cfb2db3",
+    "url": "/static/js/2.d22e0148.chunk.js"
+  },
+  {
+    "revision": "0af0e533279a0511e4ff",
+    "url": "/static/js/main.ca1d8f76.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ea35a7afdf3d631598c6650838c5856c",
+    "url": "/static/media/logo-large-complete-business.ea35a7af.png"
+  },
+  {
+    "revision": "1a77aaf9877ee243d5fa094483dd9af7",
+    "url": "/static/media/logo-large-complete-entertainment.1a77aaf9.png"
+  },
+  {
+    "revision": "b09317b63d7e80aee0b206e8b34aeef0",
+    "url": "/static/media/logo-large-complete-general.b09317b6.png"
+  },
+  {
+    "revision": "5cd23a191e109daf86b327f2f0e2d9ee",
+    "url": "/static/media/logo-large-complete-health.5cd23a19.png"
+  },
+  {
+    "revision": "ebf0e2d1fceb6597e49da4fc2aa7a8b0",
+    "url": "/static/media/logo-large-complete-science.ebf0e2d1.png"
+  },
+  {
+    "revision": "f24eb165eaf34d763390c7eeac501ad1",
+    "url": "/static/media/logo-large-complete-sports.f24eb165.png"
+  }
+]);
